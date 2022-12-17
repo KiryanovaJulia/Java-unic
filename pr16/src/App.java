9ce3407b60ac20f08014e3eed5a4b738aabe4698
YuliyaKiryanova <72200236+KiryanovaJulia@users.noreply.github.com>
2022-12-17 05:32:55 +0300

    tasks1-18

---
+++ pr16/src/App.java
@@ -0,0 +1,14 @@
+import task1.task1_1;
+import task1.task1_2;
+import rask2.task2_1;
+
+public class App {
+    public static void main(String[] args) {
+        // Игра
+        new task1_1();
+        // Мышка
+        new task1_2();
+        // Меню
+        new task2_1();
+    }
+}
